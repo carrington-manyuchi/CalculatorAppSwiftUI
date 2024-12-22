7311ac76a74758cf9ef32f1d16b6bc25d1c64d42
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-22 19:47:33 +0200

    Initial Commit

---
+++ CalculatorAppSwiftUI/CalculatorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppSwiftUIApp.swift
+//  CalculatorAppSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2024/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
