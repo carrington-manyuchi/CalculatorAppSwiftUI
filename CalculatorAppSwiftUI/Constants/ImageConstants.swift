d10d2f2661cf4973e1886f9850d3aa2c6dab3e86
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-12-28 08:09:14 +0200

    Refactor Code

---
+++ CalculatorAppSwiftUI/Constants/ImageConstants.swift
@@ -0,0 +1,24 @@
+//
+//  ImageConstants.swift
+//  CalculatorAppSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2024/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+let buttonBackgroundColor = Color("buttonBackground")
+
+let foregroundDigitsColor = Color("foregroundDigits")
+let foregroundRightButtonsColor = Color("foregroundRightButtons")
+
+let foregroundTopButtonsColor = Color("foregroundTopButtons")
+
+let primaryBackground = Color("primaryBackground")
+let secondaryBackgroundColor = Color("secondaryBackground")
+let sunOrMoonSelected = Color("sunOrMoonSelected")
+let sunOrMoonNotSelected = Color("sunOrMoonNotSelected")
+// #272B33
--- CalculatorAppSwiftUI/Views/CalcsButtonsView.swift
+++ CalculatorAppSwiftUI/Views/CalcsButtonsView.swift
@@ -23,7 +23,6 @@ struct CalcsButtonsView: View {
     @Binding var currentComputation: String
     @Binding var mainResult: String
     
-    
     let buttonData: [RowsOfCalcButtonModel] = [
         RowsOfCalcButtonModel(
             row: [
@@ -57,7 +56,7 @@ struct CalcsButtonsView: View {
             CalcButtonModel(calcButton: .four),
             CalcButtonModel(calcButton: .five),
             CalcButtonModel(calcButton: .six),
-            CalcButtonModel(calcButton: .divide,
+            CalcButtonModel(calcButton: .subtract,
                             color: foregroundRightButtonsColor
                            )
         ]),
@@ -77,7 +76,6 @@ struct CalcsButtonsView: View {
                             color: foregroundRightButtonsColor
                            )
         ]),
-        
     ]
     
     var body: some View {
@@ -86,17 +84,10 @@ struct CalcsButtonsView: View {
                 GridRow {
                     ForEach(rowOfCalcButtonsModel.row) { calcButtonModel in
                         Button {
-                            //TODO:
-                            // Logic takes place
-                            print("Button pressed")
-                            
-                            
-                            
                             buttonPressed(calcButton: calcButtonModel.calcButton)
                         } label: {
                             ButtonView(calcButton: calcButtonModel.calcButton, fgColor: calcButtonModel.color, bgColor: buttonBackgroundColor )
                         }
-
                     }
                 }
             }
@@ -109,14 +100,43 @@ struct CalcsButtonsView: View {
         switch calcButton {
             
         case .clear:
-            currentComputation = "0"
+            currentComputation = ""
             mainResult = "0"
             
         case .equal, .negative:
-            print("eq/neg")
+            if !currentComputation.isEmpty {
+                if !lastCharacterIsAnOperator(str: currentComputation) {
+                    let sign = calcButton == .negative ? -1.0 : 1.0
+                    mainResult =  formatResult(val: sign * calculateResults())
+                    
+                    if calcButton == .negative {
+                        currentComputation = mainResult
+                    }
+                }
+            }
             
         case .decimal:
-            print("dec")
+            if let lastOccurenceOfDecimal = currentComputation.lastIndex(of: ".") {
+                if lastCharacterIsDigit(str: currentComputation) {
+                    let startIndex = currentComputation.index(lastOccurenceOfDecimal, offsetBy: 1)
+                    let endIndex = currentComputation.endIndex
+                    let range = startIndex..<endIndex
+                    let rightSubString = String(currentComputation[range])
+                    ///
+                    /// Only have digits to the right "." that means do not add another "."
+                    ///  Otherwise we can add another decimal point
+                    ///
+                    if Int(rightSubString) == nil && !rightSubString.isEmpty {
+                        currentComputation += "."
+                    }
+                }
+            }  else {
+                if currentComputation.isEmpty {
+                    currentComputation += "0."
+                } else if lastCharacterIsDigit(str: currentComputation) {
+                    currentComputation += "."
+                }
+            }
             
         case .percent:
             if lastCharacterIsDigit(str: currentComputation) {
@@ -124,7 +144,6 @@ struct CalcsButtonsView: View {
             }
                         
         case .undo:
-            print("undo")
             currentComputation = String(currentComputation.dropLast())
       
         case .zero:
@@ -138,7 +157,26 @@ struct CalcsButtonsView: View {
         default:
             appendToCurrentComputation(calcButton: calcButton)
         }
-        
+    }
+    
+    //MARK: Implements actual computation
+    func calculateResults() -> Double {
+        let visibleWorkings: String = currentComputation
+        var workings = visibleWorkings.replacingOccurrences(of: "%", with: "*0.01")
+        workings = workings.replacingOccurrences(of: multiplySymbol, with: "*")
+        workings = workings.replacingOccurrences(of: divisionSymbol, with: "/")
+        ///
+        /// If we have "35." this will be replaced by "35.0"
+        ///
+        if getLastChar(str: visibleWorkings) == "." {
+            workings += "0"
+        }
+        /// KeyPoint!
+        /// Actual computation
+        ///
+        let expr = NSExpression(format: workings)
+        let exprValue = expr.expressionValue(with: nil, context: nil) as! Double
+        return exprValue
     }
     
     func appendToCurrentComputation(calcButton: CalcButton) {
